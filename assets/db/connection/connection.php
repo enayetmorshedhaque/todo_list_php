3b54dc3876c8065ced20fa94e1ab940e7c807daa
enayetmorshedhaque <enayet4419@gmail.com>
2023-12-18 00:51:34 +0600

    fetching category done.

---
+++ assets/db/connection/connection.php
@@ -0,0 +1,106 @@
+<?php
+
+require_once('./assets/db/db_config/db_config.php');
+
+class Connection
+{
+
+    public PDO $pdo;
+    public function __construct()
+    {
+        global $host, $dbname, $username, $password;
+        $this->pdo = new PDO("mysql:server=$host;dbname=$dbname", $username, $password);
+        $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+
+    public function get_categories()
+    {
+        $statement = $this->pdo->prepare("SELECT * FROM `categories`");
+        $statement->execute();
+        return $statement->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function get_used_categories()
+    {
+        $statement = $this->pdo->prepare("SELECT DISTINCT category_name FROM `task_categories`");
+        $statement->execute();
+        return $statement->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function get_card_categories()
+    {
+        $statement = $this->pdo->prepare("SELECT task_id, category_name FROM `task_categories`");
+        $statement->execute();
+        return $statement->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function add_todo_task($todo_task)
+    {
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+            // Get form data
+            $taskName = $_POST['task_name'];
+            $taskDescription = $_POST['task_description'];
+            $taskDueDate = $_POST['task_due_date'];
+            $selectedCategories = isset($_POST['category']) ? $_POST['category'] : [];
+
+            // Insert task details into the database
+            $stmt = $this->pdo->prepare("INSERT INTO todo_task (title, description, due_date, create_date) VALUES (:title, :description, :task_due_date, :create_date)");
+
+            $stmt->bindValue(':title', $taskName);
+            $stmt->bindValue(':description', $taskDescription);
+            $stmt->bindValue(':task_due_date', $taskDueDate);
+            $stmt->bindValue(':create_date', date('Y-m-d'));
+            $stmt->execute();
+
+            // Get the ID of the inserted task
+            $taskId = $this->pdo->lastInsertId();
+
+            // Insert task categories into the task_categories table
+            $stmt = $this->pdo->prepare("INSERT INTO task_categories (task_id, category_name) VALUES (:task_id, :category_name)");
+            $stmt->bindValue(':task_id', $taskId);
+
+            foreach ($selectedCategories as $categoryName) {
+                $stmt->bindValue(':category_name', $categoryName);
+                $stmt->execute();
+            }
+        }
+    }
+
+    public function get_todo_list()
+    {
+        $statement = $this->pdo->prepare("SELECT t.id, t.title, t.description, t.create_date, tc.category_name
+        FROM todo_task t
+        JOIN task_categories tc ON t.id = tc.task_id");
+        $statement->execute();
+
+        $todo_task_data = array();
+        while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+            $taskId = $row['id'];
+
+            // Check if the task already exists in the result array
+            if (!isset($todo_task_data[$taskId])) {
+                $todo_task_data[$taskId] = array(
+                    "id" => $row['id'],
+                    "title" => $row['title'],
+                    "description" => $row['description'],
+                    "create_date" => $row['create_date'],
+                    "category_name" => array(),
+                );
+            }
+
+            // Add the category to the task
+            $todo_task_data[$taskId]['category_name'][] = array("category_name" => $row['category_name']);
+        }
+
+        // Convert the associative array to indexed array
+        $todo_task_data = array_values($todo_task_data);
+
+        // Output the result
+        return $todo_task_data;
+    }
+}
+
+return new Connection();
+
+?>
---
+++ assets/db/connection/db_config.php
@@ -0,0 +1,8 @@
+<?php
+
+$host = "localhost";
+$dbname = "todo_app";
+$username = "root";
+$password = "";
+
+?>
